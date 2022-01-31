db4c3381472e0ebd6af304d50c8f63ee7f423b22
i.danilov <i.danilov@corp.mail.ru>
2022-01-31 22:01:49 +0300

    First layout

---
+++ internal/app/store/repository.go
@@ -0,0 +1,10 @@
+package store
+
+import "github.com/Blackdeswin/track-tracking/internal/app/model"
+
+// UserRepository ...
+type UserRepository interface {
+	Create(*model.User) error
+	Find(int) (*model.User, error)
+	FindByEmail(string) (*model.User, error)
+}
