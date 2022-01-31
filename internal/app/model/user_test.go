2befe144216ef2f3d3966fa211237aec6b75f26a
i.danilov <i.danilov@corp.mail.ru>
2022-01-31 22:42:33 +0300

    First layout

--- internal/app/model/user_test.go
+++ internal/app/model/user_test.go
@@ -3,7 +3,7 @@ package model_test
 import (
 	"testing"
 
-	"github.com/gopherschool/http-rest-api/internal/app/model"
+	"github.com/Blackdeswin/track-tracking/internal/app/model"
 	"github.com/stretchr/testify/assert"
 )
 
